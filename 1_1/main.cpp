0c680a7b2f47efd9c8674e6525aeb2c7daea570d
MSI\K021G1155 <01>
2022-06-03 11:14:01 +0900

    05_03. 射影変換

--- 1_1/main.cpp
+++ 1_1/main.cpp
@@ -259,14 +259,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//	{{+0.4f, -0.7f, 0.0f}, {1.0f, 1.0f}}, // 右下
 	//	{{+0.4f, +0.7f, 0.0f}, {1.0f, 0.0f}}, // 右上
 	//};
-
+	//// 頂点データ
+	//Vertex vertices[] = {
+	//	// x      y     z       u     v
+	//	{{0.0f  , 100.0f, 0.0f}, {0.0f, 1.0f}}, // 左下
+	//	{{0.0f  ,   0.0f, 0.0f}, {0.0f, 0.0f}}, // 左上
+	//	{{100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}}, // 右下
+	//	{{100.0f,   0.0f, 0.0f}, {1.0f, 0.0f}}, // 右上
+	//};	
 	// 頂点データ
 	Vertex vertices[] = {
 		// x      y     z       u     v
-		{{0.0f  , 100.0f, 0.0f}, {0.0f, 1.0f}}, // 左下
-		{{0.0f  ,   0.0f, 0.0f}, {0.0f, 0.0f}}, // 左上
-		{{100.0f, 100.0f, 0.0f}, {1.0f, 1.0f}}, // 右下
-		{{100.0f,   0.0f, 0.0f}, {1.0f, 0.0f}}, // 右上
+		{{-50.0f, -50.0f, 50.0f}, {0.0f, 1.0f}}, // 左下
+		{{-50.0f,  50.0f, 50.0f}, {0.0f, 0.0f}}, // 左上
+		{{ 50.0f, -50.0f, 50.0f}, {1.0f, 1.0f}}, // 右下
+		{{ 50.0f,  50.0f, 50.0f}, {1.0f, 0.0f}}, // 右上
 	};
 
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
@@ -711,13 +718,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		result = constBuffTransform->Map(0, nullptr, (void**)&constMapTransform); // マッピング
 		assert(SUCCEEDED(result));
 	}
-	//単位行列を代入
-	constMapTransform->mat = XMMatrixIdentity();
-	constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
-	constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;
-	constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
-	constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	////単位行列を代入
+	//constMapTransform->mat = XMMatrixIdentity();
+	//constMapTransform->mat.r[0].m128_f32[0] = 2.0f / 1280;
+	//constMapTransform->mat.r[1].m128_f32[1] = -2.0f / 720;
+	//constMapTransform->mat.r[3].m128_f32[0] = -1.0f;
+	//constMapTransform->mat.r[3].m128_f32[1] = 1.0f;
+	//// 透視投影行列の計算
+	//constMapTransform->mat = XMMatrixOrthographicOffCenterLH(2.0f/1280,-2.0f/720,-1.0f,1.0f,0.0f, 1.0f);
+	//constMapTransform->mat = XMMatrixPerspectiveFovLH(XMConvertToRadians(45.0f),(float)1280/720,0.1f,1000.0f);
 	// 値を書き込むと自動的に転送される
+
+	//射影変換行列
+	XMMATRIX matProjection = XMMatrixPerspectiveFovLH(XMConvertToRadians(45.0f),(float)window_width / window_height,0.1f,1000.0f);
+	//定数バッファ
+	constMapTransform->mat = matProjection;
 	constMapMaterial->color = XMFLOAT4(1, 1, 1, 1);              // RGBAで半透明の赤
 
 	// デスクリプタレンジの設定
